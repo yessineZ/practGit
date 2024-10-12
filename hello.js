96ac66aaac4bf8ba6a8363b40356c4878ac94c29
misty <yessine@gmail.com>
2024-10-12 14:35:54 +0100

    go to the local repo

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
 console.log("hello git") ; 
+//hello brother
+// git checkout idofCommit
+//git checkout main //to go back to the last commit
+//git checkout -f main
+//git branch misty 
+//git checkout branch-name
+//git checkout main
+//git checkout -b misty // create a branch and work on it immedietly
+//git branch new-branch-name underthetargetbranch-name   //  main  ------> branch1 
+//git push --set-upstream origin feature-branch  //push only one specific branch 
+
+
+
