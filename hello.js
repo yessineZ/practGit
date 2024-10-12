c20a6c611a79b37e5ee97104e42d326293d37ae0
misty <yessine@gmail.com>
2024-10-12 17:19:58 +0100

    modified page hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 console.log("hello git") ; 
-console.log("hello from misty69") ; 
-console.log("hello form misty69") ; 
-console.log("hello form misty69") ;
+console.log("hello from misty70") ; 
+console.log("hello form misty70") ; 
+console.log("hello form misty70") ;
 //hello brother
 // git checkout idofCommit
 //git checkout main //to go back to the last commit
@@ -12,6 +12,6 @@ console.log("hello form misty69") ;
 //git checkout -b misty // create a branch and work on it immedietly
 //git branch new-branch-name underthetargetbranch-name   //  main  ------> branch1 
 //git push --set-upstream origin feature-branch  //push only one specific branch 
-
 //text added by main2 branch
+
 
